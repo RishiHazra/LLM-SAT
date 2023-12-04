d03ab31a4b44e6934de1b1c534d1fe777f9f4e1d
Gabriele Venturato <gabriele.venturato@kuleuven.be>
2023-12-04 09:58:16 +0100

    Remove token and add change cache directory instruction

--- llama/inference.py
+++ llama/inference.py
@@ -1,10 +1,14 @@
 import torch
+import os
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+os.environ["TRANSFORMERS_CACHE"] = os.environ["VSC_SCRATCH"] + '/.cache'
+#print(os.environ["TRANSFORMERS_CACHE"])
 
 def query_llama(prompt):
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     print(device)
-    # access_token = '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'
+    access_token = 'your-token'
     model_name = "meta-llama/Llama-2-70b-chat-hf"
 
     bnb_config = BitsAndBytesConfig(
